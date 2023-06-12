ba714b21892bfd7133058782244c187b596e5bf3
mikson022 <mixkoc.21@gmail.com>
2023-06-12 14:54:13 +0200

    Add Main Menu GUI using MVC design pattern
    Implement MVC design pattern for improved code structure and maintainability
    Develop GUI Main Menu with three buttons: New Game, High Scores, and Exit
    Omit MainMenuModel class as it's not necessary

--- README.md
+++ README.md
@@ -2,14 +2,21 @@
 
 Pac-Tastic is a Java Swing application that brings back the classic arcade game Pacman. 
 It challenges players to navigate through a maze, eat pellets, avoid enemies, and collect various upgrades. 
-This project demonstrates my skills in GUI programming, event handling, serialization, and more.
+This project demonstrates my skills in GUI programming, event handling, serialization, MVC design pattern and more.
 
 ## Table of Contents
 
 - [Features](#features)
+- [Usage](#usage)
 - [License](#license)
 
 ## Features
+
+- Main menu with options for starting a new game, viewing high scores, and exiting
+
+## Usage
+
+- Navigate the main menu using the mouse and press to select an option.
 
 ## License
 
---
+++ src/controller/MainMenuController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import view.MainMenuView;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenuController implements ActionListener {
+    private MainMenuView view;
+    public MainMenuController() {
+        view = new MainMenuView();
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        view.getNewGameButton().addActionListener(this);
+        view.getHighScoreButton().addActionListener(this);
+        view.getExitButton().addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == view.getNewGameButton()) {
+            view.getNewGameButton();
+        } else if (e.getSource() == view.getHighScoreButton()) {
+            view.getHighScoreButton();
+        } else if (e.getSource() == view.getExitButton()) {
+            System.exit(0);
+        }
+    }
+}
+
+
