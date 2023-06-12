3eaaa03544ae357d15036dabb638829be88dafed
tal4va <125257322+tal4va@users.noreply.github.com>
2023-06-12 14:54:13 +0200

    Add Main Menu GUI using MVC design pattern
    Implement MVC design pattern for improved code structure and maintainability
    Develop GUI Main Menu with three buttons: New Game, High Scores, and Exit
    Omit MainMenuModel class as it's not necessary

---
+++ src/view/MainMenuView.java
@@ -0,0 +1,73 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.util.Objects;
+
+public class MainMenuView extends JFrame {
+    private JButton newGame = new JButton("New Game");
+    private JButton highScore = new JButton("High Scores");
+    private JButton exit = new JButton("Exit");
+
+    public MainMenuView() {
+        JLabel label = new JLabel();
+        this.setTitle("PACMAN");
+
+        ImageIcon pacIcon = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("pacIcon.png")));
+        this.setIconImage(pacIcon.getImage());
+
+        ImageIcon upperGreeter = new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("UpperPlaceGreeter.jpg")));
+        label.setIcon(upperGreeter);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        Border borderOfMain = BorderFactory.createLineBorder(Color.RED, 5);
+        label.setBorder(borderOfMain);
+
+        newGame.setBounds(5, 200, 988, 200);
+        highScore.setBounds(5, 400, 988, 200);
+        exit.setBounds(5, 600, 988, 200);
+
+        newGame.setFocusPainted(false);
+        highScore.setFocusPainted(false);
+        exit.setFocusPainted(false);
+
+        newGame.setBackground(Color.BLACK);
+        highScore.setBackground(Color.BLACK);
+        exit.setBackground(Color.BLACK);
+
+        newGame.setForeground(Color.RED);
+        highScore.setForeground(Color.RED);
+        exit.setForeground(Color.RED);
+
+        newGame.setToolTipText("Starts new game, you will be asked for the size of board");
+        highScore.setToolTipText("Displays high score list");
+        exit.setToolTipText("Exits the game");
+
+        newGame.setBorderPainted(false);
+        highScore.setBorderPainted(false);
+        exit.setBorderPainted(false);
+
+        Font fontForButtons = new Font("IDK", Font.ITALIC, 40);
+
+        newGame.setFont(fontForButtons);
+        highScore.setFont(fontForButtons);
+        exit.setFont(fontForButtons);
+
+        this.add(label);
+        this.add(newGame);
+        this.add(highScore);
+        this.add(exit);
+
+        this.add(label);
+        this.setSize(1000,850);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        this.setVisible(true);
+        this.getContentPane().setBackground(Color.BLACK);
+    }
+    public JButton getNewGameButton() { return newGame; }
+    public JButton getHighScoreButton() { return highScore; }
+    public JButton getExitButton() { return exit; }
+}
