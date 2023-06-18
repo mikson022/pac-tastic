fdc599296c9ca74e346673f8ea217c364cc5d5aa
mikson022 <mixkoc.21@gmail.com>
2023-06-18 18:29:12 +0200

    Implement scalability and panel below game area
    Resize to invoke scaling
    Panel not functional, view doesn't change

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -3,8 +3,11 @@ package view;
 import object.Pacman;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.Objects;
 
 public class GameView extends JFrame{
@@ -23,7 +26,7 @@ public class GameView extends JFrame{
 
         table.setBackground(Color.BLACK);
         table.setGridColor(table.getBackground());
-        for (int i = 0; i < table.getColumnCount(); i++) { table.getColumnModel().getColumn(i).setPreferredWidth(10); }
+        for (int i = 0; i < table.getColumnCount(); i++) { table.getColumnModel().getColumn(i).setPreferredWidth(20); }
 
         table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
             @Override
@@ -36,11 +39,59 @@ public class GameView extends JFrame{
                 }
             }
         });
+
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                scaleProperly();
+            }
+        });
+
         add(table);
+
+        JPanel panelSouth = new JPanel();
+        panelSouth.setLayout(new GridLayout(2, 4, 10, 10 ));
+        panelSouth.setPreferredSize(new Dimension(100, 100));
+        Border bRed = BorderFactory.createLineBorder(Color.RED, 5);
+        panelSouth.setBorder(bRed);
+        panelSouth.setBackground(Color.BLACK);
+
+
+        JLabel timeLabel = new JLabel("Time");
+        panelSouth.add(timeLabel);
+        JLabel livesLabel = new JLabel("Lives");
+        panelSouth.add(livesLabel);
+        JLabel scoreLabel = new JLabel("Score");
+        panelSouth.add(scoreLabel);
+        JLabel levLabel = new JLabel("Level");
+        panelSouth.add(levLabel);
+        // temporary
+        JLabel timeL = new JLabel("00:00");
+        panelSouth.add(timeL);
+        JLabel livesL = new JLabel("3");
+        panelSouth.add(livesL);
+        JLabel scoreL = new JLabel("0");
+        panelSouth.add(scoreL);
+        JLabel levL = new JLabel("1");
+        panelSouth.add(levL);
+
+        add(panelSouth, BorderLayout.SOUTH);
         pack();
         setLocationRelativeTo(null);
         setBackground(Color.BLACK);
         setVisible(true);
+    }
+    private void scaleProperly() {
+        int width = table.getWidth();
+        int height = table.getHeight();
+        int cellSize = Math.min(width / table.getColumnCount(), height / table.getRowCount());
+
+        table.setRowHeight(cellSize);
+        for (int i = 0; i < table.getColumnCount(); i++) { table.getColumnModel().getColumn(i).setPreferredWidth(cellSize); }
+
+        int x = pacman.getXPos() * cellSize;
+        int y = pacman.getYPos() * cellSize;
+        pacman.setBounds(x, y, cellSize, cellSize);
     }
     public void updateView() {
         table.repaint();
