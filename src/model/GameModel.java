275c55944616a1333a0cf1ea195994dff3434922
mikson022 <mixkoc.21@gmail.com>
2023-06-21 20:15:37 +0200

    Add and refactor sound components

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -9,30 +9,9 @@ import java.util.Random;
 public class GameModel {
     private final JTable table;
     private final Pacman pacman;
-    private boolean isSoundPlaying;
     public GameModel(Pacman pacman, JTable table) {
         this.pacman = pacman;
         this.table = table;
-    }
-    public void playSound(String soundName) {
-        if (!isSoundPlaying) {
-            isSoundPlaying = true;
-            try {
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getClassLoader().getResource("sounds/" + soundName)));
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                clip.addLineListener(event -> {
-                    if (event.getType() == LineEvent.Type.STOP) {
-                        isSoundPlaying = false;
-                        clip.close();
-                    }
-                });
-                clip.start();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                isSoundPlaying = false;
-            }
-        }
     }
     public int getRandomIntWithinRows() {
         Random random = new Random();
